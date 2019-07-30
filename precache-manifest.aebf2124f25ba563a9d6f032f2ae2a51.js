c3ed1feca2a9c142d609dfad5d142e3fe73614c9
musicaldoofus <burns.trainingdesign@gmail.com>
2019-07-30 00:14:49 -0700

    Updates

---
+++ precache-manifest.aebf2124f25ba563a9d6f032f2ae2a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728fbdfee6105e1d560da0bb0ef5543d",
+    "url": "/agot-deck-player/index.html"
+  },
+  {
+    "revision": "ee0d5acb3487cd11610a",
+    "url": "/agot-deck-player/static/css/main.2b1a58fe.chunk.css"
+  },
+  {
+    "revision": "f369adc043f82f517a60",
+    "url": "/agot-deck-player/static/js/2.b362cf18.chunk.js"
+  },
+  {
+    "revision": "ee0d5acb3487cd11610a",
+    "url": "/agot-deck-player/static/js/main.2837035d.chunk.js"
+  },
+  {
+    "revision": "88cdf32a727b3cc42089",
+    "url": "/agot-deck-player/static/js/runtime~main.a28b57bf.js"
+  },
+  {
+    "revision": "e3957e7bdde4794ee86ae8883305ba2b",
+    "url": "/agot-deck-player/static/media/card-back.e3957e7b.png"
+  }
+]);
