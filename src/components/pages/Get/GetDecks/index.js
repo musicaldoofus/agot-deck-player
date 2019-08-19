5147a5187a94bad13d335554234ef8e3f8c71e3c
musicaldoofus <burns.trainingdesign@gmail.com>
2019-08-19 12:20:32 -0700

    Some basic cleanup to GetDecks

--- src/components/pages/Get/GetDecks/index.js
+++ src/components/pages/Get/GetDecks/index.js
@@ -1,4 +1,4 @@
-	import React, { Component } from 'react';
+import React, { Component } from 'react';
 import CardPileBtn from '../../../atoms/CardPileBtn';
 import OverlayCardList from '../../../organisms/OverlayCardList';
 import getDeckFromAPI from '../../../../helpers/getFromAPI';
@@ -11,10 +11,10 @@ class GetDecks extends Component {
 		super(props);
 		this.state = {
 			searchInput: '',
-			decklistCache: [],
-			recentlySearchedLimit: 0,
+			searchCache: [],
+			recentlySearchedCache: [],
 			focusDeck: null,
-			startingFocusIndex: 0
+			focusDeckCacheType: 'searchCache'
 		};
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
@@ -26,8 +26,7 @@ class GetDecks extends Component {
 	componentDidMount() {
 		const recentlySearched = getFromDB('recentlySearched');
 		this.setState({
-			decklistCache: this.state.decklistCache.concat(recentlySearched),
-			recentlySearchedLimit: recentlySearched.length
+			recentlySearchedCache: this.state.recentlySearchedCache.concat(recentlySearched)
 		});
 	}
 	
@@ -41,22 +40,22 @@ class GetDecks extends Component {
 	}
 	
 	updateDecklistCache(deck) {
-		this.setState({decklistCache: this.state.decklistCache.concat(deck)});
+		this.setState({searchCache: this.state.searchCache.concat(deck)});
 		let recentlySearched = getFromDB('recentlySearched');
 		if (recentlySearched === null) recentlySearched = [];
 		recentlySearched = recentlySearched.filter(oldDeck => oldDeck.id !== deck.id); //avoid duplicates in cache
 		putToDB('recentlySearched', recentlySearched.concat(deck));
 	}
 	
-	handleClickDecklist(deckId) {
+	handleClickDecklist(deckId, focusDeckCacheType) {
 		console.log('got', deckId);
 		let focusDeck;
-		for (let i = 0; i < this.state.decklistCache.length; i++) {
-			console.log(this.state.decklistCache[i]);
-			if (this.state.decklistCache[i].id === deckId) focusDeck = i;
+		for (let i = 0; i < this.state[focusDeckCacheType].length; i++) {
+			console.log(this.state[focusDeckCacheType][i]);
+			if (this.state[focusDeckCacheType][i].id === deckId) focusDeck = i;
 		}
 		console.log('focusDeck id set to', focusDeck);
-		this.setState({focusDeck});
+		this.setState({focusDeck, focusDeckCacheType});
 	}
 	
 	handleDismiss() {
@@ -64,21 +63,27 @@ class GetDecks extends Component {
 	}
 	
 	render() {
-		const newResults = this.state.decklistCache.slice(this.state.recentlySearchedLimit, this.state.decklistCache.length - 1);
-		const oldResults = this.state.decklistCache.slice(0, this.state.recentlySearchedLimit);
-		const deckListResults = newResults.map((deck, i) => (
+		const searchResults = this.state.searchCache.map((deck, i) => (
 			<div key={i} id={`search-result-decklist-${deck.id}`}>
+				<div>
+					<h3>{deck.name}</h3>
+					<a href={`https://thronesdb.com/decklist/view/${deck.id}`} target="_blank" rel="noopener noreferrer">Go to decklist page</a>
+				</div>
 				<CardPileBtn
 					imgSrc={factionCardImages[deck.faction_code]}
-					onClick={() => this.handleClickDecklist(deck.id)}
+					onClick={() => this.handleClickDecklist(deck.id, 'searchCache')}
 				/>
 			</div>
 		));
-		const recentlySearched = oldResults.map((deck, i) => (
+		const recentlySearched = this.state.recentlySearchedCache.map((deck, i) => (
 			<div key={i} id={`search-result-decklist-${deck.id}`}>
+				<div>
+					<h3>{deck.name}</h3>
+					<a href={`https://thronesdb.com/decklist/view/${deck.id}`} target="_blank" rel="noopener noreferrer">Go to decklist page</a>
+				</div>
 				<CardPileBtn
 					imgSrc={factionCardImages[deck.faction_code]}
-					onClick={() => this.handleClickDecklist(deck.id)}
+					onClick={() => this.handleClickDecklist(deck.id, 'recentlySearchedCache')}
 				/>
 			</div>
 		));
@@ -98,20 +103,20 @@ class GetDecks extends Component {
 				</form>
 				<div id="search-results">
 					<h1>Decklist search results</h1>
-					<div style={{display: 'grid', gridTemplateColumns: 'repeat(3, auto)'}}>
-						{this.state.decklistCache.length > 0 && deckListResults}
+					<div style={{display: 'grid', gridTemplateColumns: `repeat(${this.state.searchCache.length}, 10rem)`}}>
+						{this.state.searchCache.length > 0 && searchResults}
 					</div>
 				</div>
 				<div id="recently-searched">
 					<h1>Recently searched decklists</h1>
-					<div className="decklist-viewer" style={{gridTemplateColumns: `repeat(${oldResults.length}, ${oldResults.length === 1 ? '120px' : 'auto'})`}}>
+					<div className="decklist-viewer" style={{gridTemplateColumns: `repeat(${this.state.recentlySearchedCache.length}, 10rem)`}}>
 						{recentlySearched}
 					</div>
 				</div>
 				{this.state.focusDeck !== null &&
 					<OverlayCardList
 						label=""
-						cards={this.state.decklistCache[this.state.focusDeck].cardList}
+						cards={this.state[this.state.focusDeckCacheType][this.state.focusDeck].cardList}
 						onDismiss={this.handleDismiss}
 					/>
 				}
