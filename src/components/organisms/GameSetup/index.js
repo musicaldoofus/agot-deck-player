86b0d2c6ad8f1210d7673ee4826edbe4f58ec9da
musicaldoofus <burns.trainingdesign@gmail.com>
2019-10-01 13:06:12 -0700

    Updated GameSetup to use DeckUiCard

--- src/components/organisms/GameSetup/index.js
+++ src/components/organisms/GameSetup/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import cardCache from '../../../helpers/cardCache';
-import UiCard from '../../atoms/UiCard';
+import DeckUiCard from '../../molecules/DeckUiCard';
 
 const GameSetup = (props) => {
     const deckList = cardCache.get('decklist', null, 'all');
     const decks = deckList.map(deck => (
-        <UiCard key={deck.id} className="game-setup-deck" onClick={() => props.handleSelectDeck(deck)}>
-            <div>{deck.name}</div>
-        </UiCard>
+        <DeckUiCard
+            deck={deck}
+            key={deck.id}
+            onClick={() => props.handleSelectDeck(deck)}
+        />
     ));
     return (
         <div className="game-setup">
             <h1>Select a deck to play.</h1>
-            <div className="game-setup-recent-decks">
+            <div className="deck-results-container">
                 {decks}
             </div>
         </div>
